679856a1ecdd87c9577b3fc7e392f6a889048594
alexborrazasm <alexborrazasm@gmail.com>
2026-02-27 18:04:45 +0100

    {ALL} Upload basic environment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "HACKUDC 2026 environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pyPkgs = ps: with ps; [
+        requests
+        fastapi
+        fastapi-cli
+        sqlmodel
+        pylint
+        uvicorn
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (python312.withPackages pyPkgs)
+          nodejs_22
+          sqlite
+        ]; # <-- Aquí faltaba el cierre
+
+        shellHook = ''
+          echo "HACKUDC 2026 - env loaded!"
+          echo "Python: $(python --version)"
+          echo "Node:   $(node --version)"
+        '';
+      };
+    };
+}
